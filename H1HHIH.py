915ee68513debd8adf88501388eb3f7b8993c4f9
mahmoud-gg <136716364+mahmoud-gg@users.noreply.github.com>
2023-12-14 14:42:50 +0200

    Update H1HHIH.py

--- H1HHIH.py
+++ H1HHIH.py
@@ -1,4 +1,5 @@
 import asyncio 
+import tgcrypto
 from pyrogram import Client, filters, idle
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, BotCommand
 from kvsqlite.sync import Client as DB
@@ -27,7 +28,7 @@ from pyrogram import (
 )
 
 #حقوق احمد @H1HHIH - @Z1ZZIZ
-ownerID = int("OWNER_ID") #ايدي الادمن 
+ownerID = "OWNER_ID" #ايدي الادمن 
 api_hash = "APP_HASH" #ايبي هاش 
 api_id = APP_ID #ايبي ايدي
 token = "BOT_TOKN" #توكن البوت
