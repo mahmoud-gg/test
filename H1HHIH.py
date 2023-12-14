90439e3c0889d294e4ede3a214e66818ac9051b1
mahmoud-gg <136716364+mahmoud-gg@users.noreply.github.com>
2023-12-14 15:16:59 +0200

    Update H1HHIH.py

--- H1HHIH.py
+++ H1HHIH.py
@@ -31,7 +31,7 @@ from pyrogram import (
 #حقوق احمد @H1HHIH - @Z1ZZIZ
 ownerID = "OWNER_ID" #ايدي الادمن 
 api_hash = "APP_HASH" #ايبي هاش 
-api_id = APP_ID #ايبي ايدي
+api_id = API_ID #ايبي ايدي
 token = "BOT_TOKN" #توكن البوت
 
 
