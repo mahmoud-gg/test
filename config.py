12a823d3895f9ed544910a771ae0f33ca827681f
mahmoud-gg <136716364+mahmoud-gg@users.noreply.github.com>
2023-12-14 15:10:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-c
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("APP_ID"))
+API_HASH = getenv("APP_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKN")
+OWNERID = int(getenv("OWNER_ID"))
